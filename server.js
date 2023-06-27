f305e777928ca4a6bcc31263b84b25a34adcf5df
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-06-27 12:13:59 -0400

    Create db queries

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000;
 
+const db = require('./queries')
+
+
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
@@ -13,6 +16,13 @@ app.use(
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
